0241af011dc78a153fb14ea27b054cfe7b35804b
Naberson265 <241521564+Naberson265@users.noreply.github.com>
2026-03-17 19:23:57 -0400

    4-1 Progress
    Added some new rooms, improved the first hall room, improved turret raycasting, added a spike breaking sound, and added a door that only opens if the player has a specific power up (if negated is enabled it will close when they have the power up). Gonna make UI sprites and music for the rest of the day.

--- Assets/Scripts/Triggers/DamageTrigger.cs
+++ Assets/Scripts/Triggers/DamageTrigger.cs
@@ -5,6 +5,7 @@ public class DamageTrigger : MonoBehaviour
     public int damageAmount = 1;
     public int damageLevel = 0;
     public bool ignoreIFrames = false;
+    public bool breakable = false;
 
     void OnTriggerEnter(Collider other)
     {
@@ -12,6 +13,16 @@ public class DamageTrigger : MonoBehaviour
         {
             PlayerController ps = other.transform.gameObject.GetComponent<PlayerController>();
             ps.Damage(damageAmount, damageLevel, ignoreIFrames);
+            if (breakable)
+            {
+                if (damageLevel >= 3 ||
+                damageLevel == 2 && !(ps.GetAbility() == 3 && ps.usedAirAbility) ||
+                damageLevel == 1 && ps.GetAbility() != 3)
+                {
+                    ps.playerAudio.PlayOneShot(ps.spikeBreakSFX);
+                    Destroy(gameObject);
+                }
+            }
         }
     }
 
