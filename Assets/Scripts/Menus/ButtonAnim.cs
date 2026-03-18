a54f99c3254610d424195f175069f005dbfcf09d
Naberson265 <241521564+Naberson265@users.noreply.github.com>
2026-03-17 21:46:28 -0400

    Organization and fixes
    Found the issue with the main menu button anims. Made some changes to stuff such as making the powerupspecific trigger door only since it won't be used for anything else.

--- Assets/Scripts/Menus/ButtonAnim.cs
+++ Assets/Scripts/Menus/ButtonAnim.cs
@@ -7,9 +7,11 @@ public class ButtonAnim : MonoBehaviour
     private RectTransform rectTransform;
     public Vector2 destinationPos;
     public float timeUntilAnim = 1f;
+    public float speed = 0.1f;
 
     void Start()
     {
+        Time.timeScale = 1f;
         rectTransform = GetComponent<RectTransform>();
     }
     void Update()
@@ -20,7 +22,8 @@ public class ButtonAnim : MonoBehaviour
         }
         else
         {
-            rectTransform.anchoredPosition = Vector2.Lerp(rectTransform.anchoredPosition, destinationPos, Time.deltaTime);
+            timeUntilAnim -= Time.deltaTime;
+            rectTransform.anchoredPosition = Vector2.Lerp(rectTransform.anchoredPosition, destinationPos, speed);
         }
     }
 }
