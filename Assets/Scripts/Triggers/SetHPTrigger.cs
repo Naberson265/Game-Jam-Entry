ba2805e52d9a5245e389280e1cea8d20b15e03b0
Naberson265 <241521564+Naberson265@users.noreply.github.com>
2026-03-19 19:45:00 -0400

    3/4-1
    Pushing some wip changes from our new house now. Everything with my pc seems to work, though wifi took a while to set up.

--- Assets/Scripts/Triggers/SetHPTrigger.cs
+++ Assets/Scripts/Triggers/SetHPTrigger.cs
@@ -4,9 +4,10 @@ using System.Collections.Generic;
 public class SetHPTrigger : MonoBehaviour
 {
     public List<int> hpSet = new List<int> { 0, 0, 0, 0, 0 };
+    public bool activated = false;
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.layer == 3)
+        if (other.gameObject.layer == 3 && !activated)
         {
             PlayerController ps = PlayerController.playerController;
             if (ps.health != hpSet)
