3924d23114c85bef3243c549d0428c5dd06346f5
Naberson265 <241521564+Naberson265@users.noreply.github.com>
2026-03-22 20:57:29 -0400

    Fixes
    Fixed all issues pointed out by Suave's brother, removed compression and pixel filtering from every texture, and improved the title screen.

--- Assets/Scripts/Triggers/SetHPTrigger.cs
+++ Assets/Scripts/Triggers/SetHPTrigger.cs
@@ -4,15 +4,17 @@ using System.Collections.Generic;
 public class SetHPTrigger : MonoBehaviour
 {
     public List<int> hpSet = new List<int> { 0, 0, 0, 0, 0 };
-    public bool activated = false;
+    public bool usable = true;
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.layer == 3 && !activated)
+        if (other.gameObject.layer == 3 && usable)
         {
             PlayerController ps = PlayerController.playerController;
+            List<int> copySetList = new List<int>();
+            copySetList.AddRange(hpSet);
             if (ps.health != hpSet)
             {
-                ps.health = hpSet;
+                ps.health = copySetList;
                 ps.playerAudio.PlayOneShot(ps.powerupSFX);
                 ps.UpdateAppearance();
             }
