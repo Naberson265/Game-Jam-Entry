4d854da9587294744569a43666d1fbcd76b1a2b9
Naberson265 <241521564+Naberson265@users.noreply.github.com>
2026-03-24 21:32:18 -0400

    3-3-1 Finished (?)
    Finished but not playtested.
    Spending tmrw on music.

--- Assets/Scripts/Objects/Activatable/TeleporterTrigger.cs
+++ Assets/Scripts/Objects/Activatable/TeleporterTrigger.cs
@@ -1,12 +1,31 @@
 using UnityEngine;
 using System.Collections.Generic;
 
-public class TeleporterTrigger : MonoBehaviour
+public class TeleporterTrigger : Activatable
 {
     public Vector3 destinationPos;
+    public GameObject[] glows;
+    public bool multiUse = true;
+    void Update()
+    {
+        if (activated)
+        {
+            foreach(GameObject glow in glows)
+            {
+                glow.SetActive(true);
+            }
+        }
+        else
+        {
+            foreach(GameObject glow in glows)
+            {
+                glow.SetActive(false);
+            }
+        }
+    }
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.GetComponent<Rigidbody>())
+        if (other.gameObject.GetComponent<Rigidbody>() && activated)
         {
             Rigidbody otherRb = other.gameObject.GetComponent<Rigidbody>();
             otherRb.position = destinationPos;
